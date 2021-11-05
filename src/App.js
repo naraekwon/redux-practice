b22f1caab26c62da4c6b96f3ec509cb321750746
Narae Kwon <nali4214@gmail.com>
2021-11-05 13:38:20 -0700

    reading and dispatching from a new slice

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import { Fragment } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import Counter from './components/Counter';
+import Header from './components/Header';
+import Auth from './components/Auth';
+import UserProfile from './components/UserProfile';
 
 
 function App() {
+  
+  const isAuthenticated = useSelector(state => state.auth.isAuthenticated);
+  console.log('is authenticated1', isAuthenticated);
+
   return (
-    <Counter />
+    <Fragment>
+      <Header />
+      {!isAuthenticated && <Auth />}
+      {isAuthenticated && <UserProfile />}
+      <Counter />
+    </Fragment>
   );
 }
 
